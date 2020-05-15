ade5acffbdf60e04384211122a02297b6c405fed
Kameswaran Ganesh <kameswaran1795@gmail.com>
2020-05-15 16:18:13 +0000

    Added data_service to background axios xhr request

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,14 +2,14 @@ $(document).ready(function($) {
     $("#error").toggle();
     
     var eureka_server = $("#eureka_server")[0].innerText;
-    //var data_service = $("#data_service")[0].innerText;
+    var data_service = $("#data_service")[0].innerText;
     
     var es = function() { return axios.get(eureka_server).catch((err)=>console.log(err)); }
-    //var ds = function() { return axios.get(data_service).catch((err)=>console.log(err)); }
+    var ds = function() { return axios.get(data_service).catch((err)=>console.log(err)); }
     
-    axios.all([es()])
-        .then(axios.spread(function(e) {
-            if(e.status == 200) {
+    axios.all([es(), ds()])
+        .then(axios.spread(function(e, d) {
+            if(e != undefined && d != undefined && e.status == 200 && d.status == 200) {
                 window.location.replace("/home");
             }
             else {
